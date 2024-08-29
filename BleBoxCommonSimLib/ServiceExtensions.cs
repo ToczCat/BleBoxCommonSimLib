1fc04f7cbcfcb13b9918f55e7bb89ad2201ea55b
Dariusz Garbarz <d.garbarz@ela.pl>
2024-08-29 15:01:40 +0200

    chore: add configuration provider

--- BleBoxCommonSimLib/ServiceExtensions.cs
+++ BleBoxCommonSimLib/ServiceExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using BleBoxCommonSimLib.Services;
+using BleBoxModels.Common.Models;
 using Microsoft.Extensions.DependencyInjection;
+using System.Text.Json.Nodes;
 
 namespace BleBoxCommonSimLib;
 
@@ -11,6 +13,26 @@ public static class ServiceExtensions
             .AddSingleton<IDeviceInformationService, DeviceInformationService>()
             .AddSingleton<INetworkService, NetworkService>()
             .AddSingleton<ISettingsService, SettingsService>();
+
+        return services;
+    }
+
+    public static IServiceProvider ConfigureCommonSim(
+        this IServiceProvider services,
+        string deviceName,
+        string type,
+        string apiLevel,
+        Func<SettingsBase, object> obtainFullSettings,
+        Func<JsonObject, SettingsBase> updateFullSettings,
+        string product = "commonBox")
+    {
+        var deviceService = services.GetRequiredService<IDeviceInformationService>();
+        deviceService.InitializeDevice(deviceName, type, apiLevel, product);
+
+        var settingsService = services.GetRequiredService<ISettingsService>();
+
+        settingsService.ObtainFullSettings = obtainFullSettings;
+        settingsService.UpdateFullSettings = updateFullSettings;
 
         return services;
     }
